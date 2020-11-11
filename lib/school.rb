342d15b87c87f4a107975745336f51b6f2d78660
Ghostily-Boo <grace.rl.qin@gmail.com>
2020-11-10 22:00:00 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
-# code here!
+require 'pry'
+class School
+
+    attr_reader :name
+    attr_accessor :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        if @roster.keys.include?(grade)
+            @roster[grade] << student 
+        else
+            @roster[grade] = []
+            @roster[grade] << student
+        end    
+        @roster
+    end
+
+    def grade(number)
+        @roster[number]
+    end
+
+    def sort
+        @roster.map {|key, value| @roster[key] = value.sort}
+        @roster.sort.to_h
+    end
+end
